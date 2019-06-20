32a73255127dd749c8060066cae06c24074675f2
Noam Asor <noamas@gmail.com>
2019-06-20 09:36:36 +0200

    To fix archive.appendixPattern propagation from crossBuild block level

--- src/main/groovy/com/github/prokod/gradle/crossbuild/model/ArchiveNaming.groovy
+++ src/main/groovy/com/github/prokod/gradle/crossbuild/model/ArchiveNaming.groovy
@@ -1,8 +1,15 @@
 package com.github.prokod.gradle.crossbuild.model
 
+import javax.inject.Inject
+
 /**
  * cross build plugin DSL representation for {@code archive.appendixPattern}
  */
 class ArchiveNaming {
-    String appendixPattern = '_?'
+    String appendixPattern
+
+    @Inject
+    ArchiveNaming(String appendixPattern) {
+        this.appendixPattern = appendixPattern
+    }
 }
