0af4da93d48dba452641f37aa2cfed6f01ee63fb
Noam Asor <noamas@gmail.com>
2023-04-25 23:07:45 +0200

    To add new DSL option test coverage

--- src/test/groovy/com/github/prokod/gradle/crossbuild/CrossBuildPluginExtensionTest.groovy
+++ src/test/groovy/com/github/prokod/gradle/crossbuild/CrossBuildPluginExtensionTest.groovy
@@ -67,7 +67,7 @@ class CrossBuildPluginExtensionTest extends CrossBuildGradleRunnerSpec {
      */
     @Requires({ instance.testMavenCentralAccess() })
     @Unroll
-    def "[gradle:#gradleVersion | default-scala-version:#defaultScalaVersion] applying crossbuild plugin on a multi-module project with dependency graph of depth 3 and with `withPlugin` dsl should propagate the same plugin configuration to all sub projects"(
+    def "[gradle:#gradleVersion | default-scala-version:#defaultScalaVersion] testing appendix pattern DSL option while applying crossbuild plugin on a multi-module project with dependency graph of depth 3 and with `withPlugin` dsl should propagate the same plugin configuration to all sub projects"(
             String gradleVersion,
             String defaultScalaVersion,
             String ap,
@@ -199,5 +199,162 @@ dependencies {
         '5.6.4'       | '2.11'              | '-def_?' | '-1-6-0_?' | '-2-4-0_?' | '-2-4-1_?' | '-1-6-0_?' | '-2-4-0_?' | '-2-4-1_?'
         '6.9.4'       | '2.12'              | '-def_?' | null       | null       | null       | '-def_?'   | '-def_?'   | '-def_?'
         '7.6.1'       | '2.11'              | '-def_?' | '-1-6-0_?' | '-2-4-0_?' | '-2-4-1_?' | '-1-6-0_?' | '-2-4-0_?' | '-2-4-1_?'
+    }
+
+    /**
+     *
+     * @param gradleVersion Gradle version i.e '4.2'
+     * @param defaultScalaVersion i.e '2.12.8'
+     * @param tcs Default TargetCompatibility Strategy i.e 'default'
+     * @param otcs1 Override TargetCompatibility Strategy for cross build no. 1 i.e '-x-y-z_?'
+     * @param otcs2 Override TargetCompatibility Strategy for cross build no. 2
+     * @param otcs3 Override TargetCompatibility Strategy for cross build no. 3
+     * @param oap3 Override Appendix Pattern for cross build no. 3
+     * @param etcs1 Expected TargetCompatibility Strategy for cross build no. 1
+     * @param etcs2 Expected TargetCompatibility Strategy for cross build no. 2
+     * @param etcs3 Expected TargetCompatibility Strategy for cross build no. 3
+     * @param eap3 Expected Appendix Pattern for cross build no. 3
+     */
+    @Requires({ instance.testMavenCentralAccess() })
+    @Unroll
+    def "[gradle:#gradleVersion | default-scala-version:#defaultScalaVersion] testing appendix pattern DSL option while applying crossbuild plugin on a multi-module project with dependency graph of depth 3 and with `withPlugin` dsl should propagate the same plugin configuration to all sub projects"(
+            String gradleVersion,
+            String defaultScalaVersion,
+            String tcs, ap,
+            String otcs1, String otcs2, String otcs3, String oap3,
+            String etcs1, String etcs2, String etcs3, String eap3
+    ) {
+        given:
+        // root project settings.gradle
+        settingsFile << """
+include 'lib'
+include 'lib2'
+include 'app'
+"""
+
+        buildFile << """
+plugins {
+    id 'com.github.prokod.gradle-crossbuild'
+}
+
+allprojects {
+    apply plugin: 'java'
+    group = 'com.github.prokod.it'
+    version = '1.0-SNAPSHOT'
+    
+    repositories {
+        mavenCentral()
+    }
+    
+    project.pluginManager.withPlugin('com.github.prokod.gradle-crossbuild') {
+        crossBuild {
+            
+            scalaVersionsCatalog = ['2.11':'2.11.12']
+        
+            archive {
+                appendixPattern = '${ap}'
+            }
+            targetCompatibility {
+                strategy = '${tcs}'
+            }
+            builds {
+                spark160 {
+                    scalaVersions = ['2.10']
+                    ${otcs1 != null ? 'targetCompatibility.strategy = \'' + otcs1 + '\'' : ''}
+                }
+                spark240 {
+                    scalaVersions = ['2.11']
+                    ${otcs2 != null ? 'targetCompatibility.strategy = \'' + otcs2 + '\'' : ''}
+                }
+                spark241 {
+                    scalaVersions = ['2.12']
+                        ${otcs3 != null ? 'targetCompatibility { strategy = \'' + otcs3 + '\' }' : ''}
+                        ${oap3 != null ? 'archive { appendixPattern = \'' + oap3 + '\' }' : ''}
+                }
+            }
+        }
+    }
+}
+"""
+
+        libBuildFile << """
+apply plugin: 'com.github.prokod.gradle-crossbuild'
+
+"""
+
+        lib2BuildFile << """
+apply plugin: 'com.github.prokod.gradle-crossbuild'
+
+dependencies {
+    implementation project(':lib')
+}
+"""
+
+        appBuildFile << """
+apply plugin: 'com.github.prokod.gradle-crossbuild'
+
+dependencies {
+    implementation project(':lib2')
+}
+"""
+
+        def project = ProjectBuilder.builder().build()
+        // Before instantiating CrossBuildExtension, project should contain sourceSets otherwise, CrossBuildSourceSets
+        // instantiation will fail.
+        project.pluginManager.apply(JavaPlugin)
+
+        ObjectFactory objects = project.services.get(ObjectFactory)
+        def build1 = new Build('spark160', new CrossBuildExtension(project, objects)).with { b ->
+            scalaVersions = ['2.10']
+            archive.appendixPattern = ap
+            targetCompatibility.strategy = etcs1
+            b
+        }
+        def build2 = new Build('spark240', new CrossBuildExtension(project, objects)).with { b ->
+            scalaVersions = ['2.11']
+            archive.appendixPattern = ap
+            targetCompatibility.strategy = etcs2
+            b
+        }
+        def build3 = new Build('spark241', new CrossBuildExtension(project, objects)).with { b ->
+            scalaVersions = ['2.12']
+            targetCompatibility.strategy = etcs3
+            archive.appendixPattern = eap3
+            b
+        }
+        def rb1 = BuildResolver.resolve(build1, ScalaVersions.withDefaultsAsFallback('2.11': '2.11.12'))
+        def rb2 = BuildResolver.resolve(build2, ScalaVersions.withDefaultsAsFallback('2.11': '2.11.12'))
+        def rb3 = BuildResolver.resolve(build3, ScalaVersions.withDefaultsAsFallback('2.11': '2.11.12'))
+
+        def expectedReport = '[' + (rb1 + rb2 + rb3).collect { it.toString() }.join(',\n') + ']'
+
+        when:
+        def result = GradleRunner.create()
+                .withGradleVersion(gradleVersion)
+                .withProjectDir(dir.toFile())
+                .withPluginClasspath()
+        /*@withDebug@*/
+                .withArguments('crossBuildResolvedDsl', '--info', '--stacktrace')
+                .build()
+
+        then:
+        result.task(":crossBuildResolvedDsl").outcome == SUCCESS
+        result.task(":lib:crossBuildResolvedDsl").outcome == SUCCESS
+        result.task(":lib2:crossBuildResolvedDsl").outcome == SUCCESS
+        result.task(":app:crossBuildResolvedDsl").outcome == SUCCESS
+
+        when:
+        def appBuildsReportFile = findFile("*/app_builds.json")
+
+        then:
+        appBuildsReportFile != null
+        appBuildsReportFile.text == expectedReport
+
+        where:
+        gradleVersion | defaultScalaVersion | tcs       | ap       | otcs1      | otcs2      | otcs3      | oap3       | etcs1       | etcs2       | etcs3       | eap3
+        '5.6.4'       | '2.10'              | 'default' | '_?'     | null       | null       | null       | null       | 'default'   | 'default'   | 'default'   | '_?'
+        '6.9.4'       | '2.11'              | 'default' | '_?'     | 'smart'    | null       | null       | null       | 'smart'     | 'default'   | 'default'   | '_?'
+        '7.6.1'       | '2.12'              | 'default' | '-def_?' | 'smart'    | 'strict'   | null       | null       | 'smart'     | 'strict'    | 'default'   | '-def_?'
+        '8.0.2'       | '2.12'              | 'default' | '-def_?' | 'smart'    | 'strict'   | null       | '-2-4-1_?' | 'smart'     | 'strict'    | 'default'   | '-2-4-1_?'
     }
 }
